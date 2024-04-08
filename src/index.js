cd0a9e6fc96d66fb0ad104e86882f8f1db6486f1
Fernando De Jesus <fdejesus95@outlook.com>
2024-04-08 13:03:45 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchDog(){
+    fetch ("https://dog.ceo/api/breeds/image/random/4")
+    .then (resp => resp.json())
+    .then (json => fetchDogPhotos(json))
+}
+
+function fetchDogPhotos(images){
+    const imgContainer = document.getElementById("dog-image-container");
+    images.message.forEach(image => {
+        const img = document.createElement('img')
+        img.src = image
+        imgContainer.appendChild(img)
+        console.log(image)
+    });
+
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetchDog();
+})
